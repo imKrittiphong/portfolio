6f62224d3d061ae5e25ecfd40ab0c49fb5e8acec
imKrittiphong <im.krittipbutsaba29@gmail.com>
2026-02-02 13:28:50 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
+return <>
+</>;
 }
 
-export default App
+export default App;
